6c20a95fd99c00a2e5402c1aa5f6f85fa99985d1
Benjamin Mortimer <benjamin.mortimer@live.com>
2020-04-01 18:34:00 +0100

    Add a form that writes to a file

--- hattify.rb
+++ hattify.rb
@@ -8,3 +8,9 @@ set :views, "views"
 get '/' do
 	erb :index
 end
+
+post '/form' do 
+	form_data = params["form_data"]
+	File.open("./data/data.txt", "a") { |file| file.puts(form_data) }
+	redirect to '/'
+end
