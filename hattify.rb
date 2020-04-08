9873d06bff5c48f380a8a52f22f669b52f6cb292
Benjamin Mortimer <benjamin.mortimer@live.com>
2020-04-08 23:58:02 +0100

    Remove non-ascii character

--- hattify.rb
+++ hattify.rb
@@ -35,7 +35,7 @@ class Game
 	attr_writer :turn_name
 
 	def initialize
-		@to_do = ["Ajit Wadekar", "Emperor Go-Saga", "Sydney Newman", "Toshiro Mifune", "Gideon Gadot", "Samuel Alito", "Loris Kessel", "Daniel Paillé",  "John Abizaid", "Frederic Schwartz", "Annette O'Toole",  "Susan Boyle", "Samboy Lim", "Ding Junhui"]
+		@to_do = ["Ajit Wadekar", "Emperor Go-Saga", "Sydney Newman", "Toshiro Mifune", "Gideon Gadot", "Samuel Alito", "Loris Kessel", "Daniel Paille",  "John Abizaid", "Frederic Schwartz", "Annette O'Toole",  "Susan Boyle", "Samboy Lim", "Ding Junhui"]
 		@done = []
 		@passes = []
 		@turn_name = new_card
