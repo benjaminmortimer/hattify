88334adfbac0896afb4f9624e6adac0443280eeb
Benjamin Mortimer <benjamin.mortimer@live.com>
2020-05-16 00:34:11 +0100

    Alphabetise routes and methods

--- hattify.rb
+++ hattify.rb
@@ -18,6 +18,10 @@ class Game
 	attr_reader :turn_name, :to_do, :done, :passes, :turn_done
 	attr_writer :turn_name
 
+	def add_name(name)
+		@to_do << name 
+	end
+
 	def initialize(to_do, done)
 		@to_do = to_do
 		@done = done
@@ -26,33 +30,20 @@ class Game
 		@turn_name
 	end
 
-	def no_names
-		[@to_do, @done, @passes].all? {|states| states.empty?}
-	end
-
-	def new_card
-		@to_do[rand(0..@to_do.length - 1)]
-	end
-
 	def guessed(card)
 		@turn_done << card
 		@done << card 
 		@to_do.delete(card)
 	end
 
-	def pass(card)
-		@passes << card 
-		@to_do.delete(card)
+	def new_card
+		@to_do[rand(0..@to_do.length - 1)]
 	end
 
-	def reset_passes
-		@passes.each { |name| @to_do << name }
-		@passes = []
-	end
-
-	def reset_done
-		@done.each { |name| @to_do << name }
-		@done = []
+	def new_round
+		reset_passes
+		reset_done
+		@turn_done = []
 	end
 
 	def new_turn
@@ -62,16 +53,24 @@ class Game
 		@turn_name = new_card
 	end
 
-	def new_round
-		reset_passes
-		reset_done
-		@turn_done = []
+	def no_names
+		[@to_do, @done, @passes].all? {|states| states.empty?}
 	end
 
-	def add_name(name)
-		@to_do << name 
+	def pass(card)
+		@passes << card 
+		@to_do.delete(card)
 	end
 
+	def reset_done
+		@done.each { |name| @to_do << name }
+		@done = []
+	end
+
+	def reset_passes
+		@passes.each { |name| @to_do << name }
+		@passes = []
+	end
 end
 
 def clean_input(input)
@@ -90,17 +89,8 @@ get '/add-names' do
 	erb :add_names
 end
 
-get '/new-turn' do 
-	if game.to_do.empty?
-		redirect to '/empty'
-	else
-	game.new_turn
-	redirect to '/turn'
-	end
-end
-
-get '/turn' do 
-	erb :turn, :locals => {:current_name => game.turn_name, :passes => game.passes, :to_do => game.to_do, :turn_done => game.turn_done}
+get '/empty' do 
+	erb :empty, :locals => {:turn_done => game.turn_done, :no_names => game.no_names}
 end
 
 get '/guessed' do
@@ -115,6 +105,36 @@ get '/guessed' do
 	game.turn_name = game.new_card
 	redirect to '/turn'
 	end
+end
+
+get '/name-added' do 
+	erb :name_added
+end
+
+post '/new-name' do
+	new_name = clean_input(params[:new_name])
+	game.add_name(new_name)
+	redirect to '/name-added'
+end
+
+get '/new-round' do 
+	game.new_round
+	redirect to '/'
+end
+
+get '/new-turn' do 
+	if game.to_do.empty?
+		redirect to '/empty'
+	else
+	game.new_turn
+	redirect to '/turn'
+	end
+end
+
+get '/next-player' do 
+	game.reset_passes
+	game.new_turn
+	redirect to '/'
 end
 
 get '/pass' do 
@@ -132,35 +152,10 @@ get '/play-pass' do
 	redirect to '/turn'
 end
 
-get '/next-player' do 
-	game.reset_passes
-	game.new_turn
-	redirect to '/'
-end
-
-get '/empty' do 
-	erb :empty, :locals => {:turn_done => game.turn_done, :no_names => game.no_names}
-end
-
-get '/new-round' do 
-	game.new_round
-	redirect to '/'
-end
-
 get '/reveal' do 
 	erb :reveal, :locals => {:to_do => game.to_do, :done => game.done, :passes => game.passes}
 end
 
-get '/add-names' do
-	erb :add_names
-end
-
-post '/new-name' do
-	new_name = clean_input(params[:new_name])
-	game.add_name(new_name)
-	redirect to '/name-added'
-end
-
-get '/name-added' do 
-	erb :name_added
+get '/turn' do 
+	erb :turn, :locals => {:current_name => game.turn_name, :passes => game.passes, :to_do => game.to_do, :turn_done => game.turn_done}
 end
